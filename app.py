2703eae87b3ae49888e0df1252b4565e3c63f19a
EnzoCorainiBarbieri <enzo.barbieri316@al.unieduk.com.br>
2024-09-18 10:28:03 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
